3eeabcc0d28e62719f39ba9fb94643a581de44d5
unknown <ajaypalsingh82775@gmail.com>
2025-08-14 20:46:18 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -1,4 +1,9 @@
+import express from "express";
 import cors from "cors";
+
+
+const app = express();
+
 app.use(cors({origin: "https://isro-game",
     methods:["GET", "POST","DELETE"],
     credentials:true
