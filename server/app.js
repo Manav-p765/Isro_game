48fd4a0c650d4503a54cff440cf63d8d08c96db9
unknown <ajaypalsingh82775@gmail.com>
2025-08-14 20:54:18 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -8,13 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Simple route
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(8080, () => {
+  console.log(`Server running on port `);
 });
