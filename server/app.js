a408927a0e3300d13bf3605edb6f72dfff4e9f12
unknown <ajaypalsingh82775@gmail.com>
2025-08-18 21:46:12 +0530

    ADD LOGIN AND SIGNUP

--- server/app.js
+++ server/app.js
@@ -1,16 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../clients/views"));
 
-app.get("/", (req, res) => {
-  res.send("ISRO Spacecraft Builder API is running 🚀");
+app.get("/login", (req, res) => {
+  res.render("users/login"); 
+});
+app.get("/signup", (req, res) => {
+  res.render("users/signup"); 
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
